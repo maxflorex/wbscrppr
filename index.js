cd542fcd6c3a2ec7ff96b9cc6ac9a7dd130d4a16
Max Flores <max@maxflores.dev>
2022-11-15 06:36:05 -0600

    Cheerio Web Scrapper

---
+++ index.js
@@ -0,0 +1,26 @@
+const axios = require('axios')
+const express = require('express')
+const cheerio = require('cheerio')
+
+const app = express()
+const PORT = 8000
+const url = 'https://www.theguardian.com/uk'
+
+axios(url).then(response => {
+    const html = response.data
+    const $ = cheerio.load(html)
+    const articles = []
+
+    $('.fc-item__title', html).each(function () {
+        const title = $(this).text()
+        const url = $(this).find('a').attr('href')
+        articles.push({
+            title,
+            url
+        })
+    })
+
+    console.log(articles);
+}).catch(err => console.log(err))
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
